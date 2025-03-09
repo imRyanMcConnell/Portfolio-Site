49be2b4de6d506029ac3421ccc9f11b7605020c2
imRyanMcConnell <r.mcconnell@myyahoo.com>
2025-03-08 22:23:34 -0800

    Added name logo with transitions.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const wrapper = document.querySelector(".content-wrapper");
 const sections = document.querySelectorAll(".section");
 const buttons = document.querySelectorAll("nav button");
+const heroContainer = document.querySelector(".hero-container")
 console.log(buttons);
 
 buttons[2].style.color = "#9883E5";
@@ -18,6 +19,14 @@ function navigateTo(index) {
         wrapper.style.transform = `translateX(${positions[index]}vw)`;
         console.log(`Applying transform: translateX(${positions[index]}vw)`);
     }
+
+    // Show the hero container when not on index 0
+    if (index != 2) {
+        heroContainer.style.transform = "translateX(510px)"
+    } else {
+        heroContainer.style.transform = "translateX(-510px)"
+    }
+    
 
     // Apply selected color to the clicked button
     buttons[index].style.color = "#9883E5";
